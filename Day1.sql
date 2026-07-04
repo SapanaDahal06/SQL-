58b7eeacdfab2a01b21c7de0e77b0f973f419599
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-04 12:23:57 +0545

    upload file

---
+++ Day1.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE itme;
+GO
+
+USE itme;
+GO
+
+CREATE TABLE Customer(
+    ID INT PRIMARY KEY,
+    Name VARCHAR(60),
+    Age INT NOT NULL,
+    City VARCHAR(50),
+    Salary NUMERIC(10,2)
+);
+
+INSERT INTO Customer (ID, Name, Age, City, Salary)
+VALUES
+(1, 'SAM', 23, 'DUBAI', 90000),
+(2, 'Nargish', 24, 'America', 100000),
+
+INSERT INTO Customer (ID, Name, Age, City, Salary)
+VALUES
+(3, 'John', 30, 'London', 120000),
+(4, 'Emma', 28, 'New York', 95000);
+
+
+-- Display Table
+SELECT * FROM Customer;
+
+DELETE FROM Customer;
+
