f268ffd8d6c4c42f7a74a82505e9d75ba45b6a21
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-06 11:53:48 +0545

    okay

--- Timestamps.sql
+++ Timestamps.sql
@@ -5,3 +5,7 @@ SELECT CAST(GETDATE() AS DATE) AS CurrentDate;
 
 -- Current Date and Time
 SELECT GETDATE() AS CurrentDateTime;
+
+
+-- Current Date, Time, and Time Zone Offset
+SELECT SYSDATETIMEOFFSET() AS CurrentDateTimeWithTimeZone;
