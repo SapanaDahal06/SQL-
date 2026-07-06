c6642aa2b1e0a8c407fb66fae9ff2e438af98df8
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-06 11:46:46 +0545

    mja

--- Timestamps.sql
+++ Timestamps.sql
@@ -1,1 +1,12 @@
 use Company;
+SELECT CURRENT_DATE;
+
+SELECT CURRENT_TIMEZONE();
+
+SELECT GETDATE();
+
+SELECT FORMAT(GETDATE(), 'dd-MM-yyyy');
+
+-- to know exact time 
+
+SELECT GETDATE() AS CurrentDateTime;
