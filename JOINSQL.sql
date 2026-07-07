8d75fec81bcd20c7a2754eec57d5b7427db74bcb
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-07 15:04:25 +0545

    okay done

--- JOINSQL.sql
+++ JOINSQL.sql
@@ -1,29 +1,36 @@
-use Company;
+USE Company;
+GO
 
+-- Student Table
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
     StudentName VARCHAR(50),
-    City VARCHAR(30)
+    City VARCHAR(30),
+    Age INT
 );
 
 INSERT INTO Student
 VALUES
-(1,'Sapana','Kathmandu'),
-(2,'Purnima','Pokhara'),
-(3,'Ramesh','Butwal'),
-(4,'Sita','Dharan');
+(1, 'Sapana', 'Kathmandu', 22),
+(2, 'Purnima', 'Pokhara', 21),
+(3, 'Ramesh', 'Butwal', 23),
+(4, 'Sita', 'Dharan', 20);
 
+-- Course Table
 CREATE TABLE Course (
     StudentID INT,
+    StudentName VARCHAR(50),
     CourseName VARCHAR(50),
     Marks INT
 );
 
 INSERT INTO Course
 VALUES
-(1, 'SQL', 92),
-(2, 'Python', 85),
-(3, 'Java', 78),
-(5, 'Django', 90);
+(1, 'Sapana', 'SQL', 92),
+(2, 'Purnima', 'Python', 85),
+(3, 'Ramesh', 'Java', 78),
+(5, 'Hari', 'Django', 90);
 
+-- Display Tables
+SELECT * FROM Student;
 SELECT * FROM Course;
