ea57b2ea6dea486575adebb4d3d2a15e11b21aba
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-07 15:16:26 +0545

    okay

--- JOINSQL.sql
+++ JOINSQL.sql
@@ -23,3 +23,11 @@ CREATE TABLE Course (
     CourseName VARCHAR(50),
     Marks INT
 );
+
+INSERT INTO Course
+VALUES
+(1, 'Sapana', 'SQL', 92),
+(2, 'Purnima', 'Python', 85),
+(3, 'Ramesh', 'Java', 78),
+(5, 'Hari', 'Django', 90),
+(6, 'Sarina', 'C++', 88);
