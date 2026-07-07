eba4f255c2f129401cef46bd40b227064e332800
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-07 14:44:52 +0545

    okay

--- JOINSQL.sql
+++ JOINSQL.sql
@@ -1,1 +1,14 @@
 use Company;
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(50),
+    City VARCHAR(30)
+);
+
+INSERT INTO Student
+VALUES
+(1,'Sapana','Kathmandu'),
+(2,'Purnima','Pokhara'),
+(3,'Ramesh','Butwal'),
+(4,'Sita','Dharan');
