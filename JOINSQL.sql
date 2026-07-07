f11c45679979b3bf587831eb49078ff489e94fb0
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-07 15:14:03 +0545

    okay

--- JOINSQL.sql
+++ JOINSQL.sql
@@ -6,3 +6,11 @@ CREATE TABLE Student (
     City VARCHAR(30),
     Age INT
 );
+
+INSERT INTO Student
+VALUES
+(1, 'Sapana', 'Kathmandu', 22),
+(2, 'Purnima', 'Pokhara', 21),
+(3, 'Ramesh', 'Butwal', 23),
+(4, 'Sita', 'Dharan', 20),
+(5, 'Hari', 'Biratnagar', 24);
