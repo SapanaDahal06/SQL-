4454160f051061c98b89932b5a17211020d7a5ba
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-07 15:34:50 +0545

    okay

--- JOINSQL.sql
+++ JOINSQL.sql
@@ -1,5 +1,5 @@
+
 use Company;
--- Student Table
 CREATE TABLE Student (
     StudentID INT PRIMARY KEY,
     StudentName VARCHAR(50),
@@ -13,30 +13,6 @@ VALUES
 (2, 'Purnima', 'Pokhara', 21),
 (3, 'Ramesh', 'Butwal', 23),
 (4, 'Sita', 'Dharan', 20),
-(5, 'Hari', 'Biratnagar', 24);
+(5, 'Priya', 'Gaighat', 25);
 
-
--- Course Table
-CREATE TABLE Course (
-    StudentID INT,
-    StudentName VARCHAR(50),
-    CourseName VARCHAR(50),
-    Marks INT
-);
-
-INSERT INTO Course
-VALUES
-(1, 'Sapana', 'SQL', 92),
-(2, 'Purnima', 'Python', 85),
-(3, 'Ramesh', 'Java', 78),
-(5, 'Hari', 'Django', 90),
-(6, 'Sarina', 'C++', 88);
-
-SELECT * FROM Student
-SELECT * FROM Course
-
---INNER JOIN 
-SELECT *
-FROM Student
-INNER JOIN Course
-ON Student.StudentID = Course.StudentID
+SELECT * FROM Student;
