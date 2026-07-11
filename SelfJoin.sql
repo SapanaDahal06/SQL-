fa0dfa848828815f99ae90f7d07076e7c179088e
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-11 14:04:32 +0545

    jja

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -6,16 +6,12 @@ EmpName VARCHAR(50),
 ManagerID int
 );
 SELECT * FROM Employe;
+
+
 Insert  into Employe 
 Values
-(1, 'sherya',10),
-(2,'prabisha',20),
-(3, 'Anshula' , 30),
-(4, 'Khusi',40);
+(1, 'sherya',NULL),
+(2,'prabisha',2),
+(3, 'Anshula' , 2),
+(4, 'Khusi',1);
 
-SELECT
-    E.EmpName AS Employee,
-    M.EmpName AS Manager
-FROM Employee E
-LEFT JOIN Employee M
-ON E.ManagerID = M.EmpID;
