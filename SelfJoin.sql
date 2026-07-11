695a9e91d67156b1d9c6e01753148c59ea4bc3d9
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-11 14:00:26 +0545

    kako

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -8,5 +8,7 @@ ManagerID int
 SELECT * FROM Employe;
 Insert  into Employe 
 Values
-(1, 'sherya',10)
-(2,'prabisha',20)
+(1, 'sherya',10),
+(2,'prabisha',20),
+(3, 'Anshula' , 30),
+(4, 'Khusi',40);
