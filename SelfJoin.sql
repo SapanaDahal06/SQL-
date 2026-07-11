49dbdf58e3067b59c52ed2a2e7a7dd36fdf01e8c
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-11 14:02:13 +0545

    ooa

--- SelfJoin.sql
+++ SelfJoin.sql
@@ -12,3 +12,10 @@ Values
 (2,'prabisha',20),
 (3, 'Anshula' , 30),
 (4, 'Khusi',40);
+
+SELECT
+    E.EmpName AS Employee,
+    M.EmpName AS Manager
+FROM Employee E
+LEFT JOIN Employee M
+ON E.ManagerID = M.EmpID;
