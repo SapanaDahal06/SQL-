84479247374833f9ff7776896434cf8a2c2c6581
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-15 00:56:22 +0545

    crete table

---
+++ CASEX.sql
@@ -0,0 +1,12 @@
+create Database ABC;
+--CASE EXPRESION 
+-- Create Employee Table
+CREATE TABLE Employee2 (
+    EmpID INT PRIMARY KEY,
+    EmpName VARCHAR(50),
+    Department VARCHAR(30),
+    City VARCHAR(30),
+    Gender CHAR(1),
+    Age INT,
+    Salary INT
+);
