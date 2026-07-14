44c96fb7fe4c0d60f337eb1d4e2227451505732f
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-15 01:11:12 +0545

    oakoj

--- CASEX.sql
+++ CASEX.sql
@@ -1,5 +1,6 @@
-create Database ABC;
---CASE EXPRESION 
+
+USE ABC;
+
 -- Create Employee Table
 CREATE TABLE Employee2 (
     EmpID INT PRIMARY KEY,
@@ -11,16 +12,19 @@ CREATE TABLE Employee2 (
     Salary INT
 );
 
-INSERT INTO Employee2 
-VALUES 
+INSERT INTO Employee2
+VALUES
 (101, 'Alice',   'IT',        'Kathmandu', 'F', 24, 85000),
 (102, 'Bob',     'IT',        'Pokhara',   'M', 27, 72000),
 (103, 'Charlie', 'HR',        'Butwal',    'M', 30, 58000),
 (104, 'Diana',   'HR',        'Kathmandu', 'F', 26, 61000),
 (105, 'Ethan',   'Sales',     'Biratnagar','M', 29, 68000),
 (106, 'Fiona',   'Sales',     'Pokhara',   'F', 25, 68000),
-
 (107, 'George',  'Finance',   'Dharan',    'M', 31, 93000),
 (108, 'Helen',   'Finance',   'Kathmandu', 'F', 28, 93000),
 (109, 'Ian',     'Marketing', 'Butwal',    'M', 26, 55000),
 (110, 'Julia',   'Marketing', 'Pokhara',   'F', 23, 55000);
+
+SELECT * FROM Employee2;
+
+--case expression 
