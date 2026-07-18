0b1d3b292769ff90b01a9e3c2bcfe8a2fd31b269
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-18 16:06:20 +0545

    kjaj

--- RecursiveCTE.sql
+++ RecursiveCTE.sql
@@ -3,3 +3,16 @@ use ABC;
 --------------------------------------------------
 
 SELECT 2 + 1 AS Number;
+-- Lesson 2: Normal CTE
+--------------------------------------------------
+
+WITH Number AS
+(
+    SELECT 1 AS Number
+    UNION
+    SELECT 2
+    UNION
+    SELECT 3
+)
+SELECT *
+FROM Number;
