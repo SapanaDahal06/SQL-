c89ce01d8a95764bffd297858ef5cd4b09401f7f
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-07-18 16:05:07 +0545

    jaj

--- RecursiveCTE.sql
+++ RecursiveCTE.sql
@@ -1,23 +1,5 @@
 use ABC;
-SELECT 2 + 1  AS Number;
+-- Lesson 1: Simple SELECT
+--------------------------------------------------
 
-WITH Number AS 
-(
-SELECT 1 AS Number
-UNION 
-SELECT 2
-UNION 
-SELECT 3
-)
-
-SELECT * 
-FROM Number;
-
-
-WITH Number AS
-(
-SELECT 1 As Number 
-UNION ALL 
-SELECT Number + 1 
-FROM Number
-)
+SELECT 2 + 1 AS Number;
