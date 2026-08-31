6aefb7fd8109af678096e291ed0b9367072325e3
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-08-31 16:29:32 +0545

    another file

---
+++ SQLQuery26.sql
@@ -0,0 +1,18 @@
+use sapana;
+CREATE TABLE em (
+    id INT,
+    Name VARCHAR(40),
+    Department VARCHAR(50),
+    Salary INT
+);
+
+INSERT INTO em
+VALUES 
+(1, 'Ram', 'IT', 500000),
+(2, 'Sita', 'HR', 600000),
+(3, 'Hari', 'IT', 800000),
+(4, 'Gita', 'Finance', 900000);
+SELECT * FROM em;
+
+select * from em
+ORDER BY  Salary ASC; 
