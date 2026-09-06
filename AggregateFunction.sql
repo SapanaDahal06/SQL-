224abe056205cca827c1af770f1df0e5a2e0f65f
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-09-06 18:39:12 +0545

    finally

--- AggregateFunction.sql
+++ AggregateFunction.sql
@@ -1,16 +1,18 @@
-use sapana;
-create table LEARNER(
+USE sapana;
 
-ID INT,
-NAME VARCHAR(90),
-AGE INT,
-COURSE VARCHAR(88),
-MARKS INT
+CREATE TABLE LEA (
+    ID INT,
+    NAME VARCHAR(90),
+    AGE INT,
+    COURSE VARCHAR(88),
+    MARKS INT
 );
 
-INSERT  INTO LEARNER ( ID,NAME,AGE,COURSE,MARKS) 
+INSERT INTO LEA (ID, NAME, AGE, COURSE, MARKS) 
 VALUES 
-(1, 'Ram',24 ,'BCA',87),
-(2,'Hari',31 ,'MBA' , 49),
-(3,'Gita',25 , 'DE' ,88),
-(4,'Muna',22 ,'BBA',78);
+(1, 'Ram', 24, 'BCA', 87),
+(2, 'Hari', 31, 'MBA', 49),
+(3, 'Gita', 25, 'DE', 88),
+(4, 'Muna', 22, 'BBA', 78);
+
+SELECT * FROM LEA;
