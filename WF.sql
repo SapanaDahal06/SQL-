73b3a82d42a61955079eaee413c996de3f853c97
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-09-08 15:45:56 +0545

    jahf

--- WF.sql
+++ WF.sql
@@ -7,3 +7,14 @@ CREATE TABLE EMP (
     DEPARTMENT VARCHAR(50),
     SALARY INT
 );
+
+INSERT INTO EMP (EMP_ID, EMP_NAME, DEPARTMENT, SALARY)
+VALUES
+(1, 'Ram', 'IT', 60000),
+(2, 'Hari', 'IT', 75000),
+(3, 'Gita', 'HR', 50000),
+(4, 'Sita', 'HR', 65000),
+(5, 'Muna', 'IT', 75000),
+(6, 'Rita', 'Finance', 55000),
+(7, 'Kiran', 'Finance', 70000),
+(8, 'Farhana','HR',80000);
