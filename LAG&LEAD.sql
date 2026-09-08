f03352bde821d80310edde73ccec65b3cfcda301
SapanaDahal06 <Sapanaa1206@gmail.com>
2026-09-08 17:22:15 +0545

    makf

--- LAG&LEAD.sql
+++ LAG&LEAD.sql
@@ -1,1 +1,20 @@
 use sapana;
+
+
+ CREATE TABLE SAPPU (
+    EMP_ID INT,
+    EMP_NAME VARCHAR(50),
+    DEPARTMENT VARCHAR(50),
+    SALARY INT
+);
+
+INSERT INTO SAPPU(EMP_ID, EMP_NAME, DEPARTMENT, SALARY)
+VALUES
+(1, 'Ram', 'IT', 60000),
+(2, 'Hari', 'IT', 75000),
+(3, 'Gita', 'HR', 50000),
+(4, 'Sita', 'HR', 65000),
+(5, 'Muna', 'IT', 75000),
+(6, 'Rita', 'Finance', 55000),
+(7, 'Kiran', 'Finance', 70000),
+(8, 'Farhana','HR',80000);
